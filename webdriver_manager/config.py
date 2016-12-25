f93d023b52c3884195522477c44266c51c0358e1
sergey <sergey@gmail.com>
2016-12-25 09:52:44 +0200

    # config.py added

---
+++ webdriver_manager/config.py
@@ -0,0 +1,2 @@
+mozila_latest_release = "https://api.github.com/repos/mozilla/geckodriver/releases/latest"
+mozila_release_tag = "https://api.github.com/repos/mozilla/geckodriver/releases/tags/{0}"
--- webdriver_manager/driver.py
+++ webdriver_manager/driver.py
@@ -1,4 +1,5 @@
 import requests
+import config
 
 from webdriver_manager.utils import OSUtils
 
@@ -39,15 +40,15 @@ class FireFoxDriver(Driver):
         super(FireFoxDriver, self).__init__(driver_url, name, version)
 
     def get_latest_release_version(self):
-        resp = requests.get("https://api.github.com/repos/mozilla/geckodriver/releases/latest")
+        resp = requests.get(config.mozila_latest_release)
         return resp.json()["tag_name"]
 
     def get_url(self):
         # https://github.com/mozilla/geckodriver/releases/download/v0.11.1/geckodriver-v0.11.1-linux64.tar.gz
-        resp = requests.get("https://api.github.com/repos/mozilla/geckodriver/releases/tags/{0}".format(self.get_version()))
+        resp = requests.get(config.mozila_release_tag.format(self.get_version()))
         os = OSUtils.os_name() + str(OSUtils.os_architecture())
         assets = resp.json()["assets"]
         ver = self.get_version()
-        name = self.name + "-" + ver + "-" + os
+        name = "{0}-{1}-{2}".format(self.name, ver, os)
         output_dict = [asset for asset in assets if asset['name'].startswith(name)]
         return output_dict[0]['browser_download_url']
