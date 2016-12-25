c49c03faed1cfc7cf2660475d28d70002ddfd0c4
sergey <sergey@gmail.com>
2016-12-25 17:27:04 +0200

    os added to DriverConstructor

--- webdriver_manager/firefox.py
+++ webdriver_manager/firefox.py
@@ -1,9 +1,9 @@
 from webdriver_manager.driver import FireFoxDriver
 from webdriver_manager.manager import DriverManager
+from webdriver_manager.utils import OSUtils
 
 
 class GeckoDriverManager(DriverManager):
-
     def __init__(self, version="latest",
                  name="geckodriver",
                  url="https://github.com/mozilla/geckodriver/releases/download"):
@@ -11,8 +11,7 @@ class GeckoDriverManager(DriverManager):
         self.driver = FireFoxDriver(driver_url=url,
                                     name=name,
                                     version=version,
-                                    )
+                                    os=OSUtils.os_type())
 
     def install(self):
         return self._file_manager.download_driver(self.driver)
-
