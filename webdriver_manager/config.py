c70e38ea57728c32a2459e4c3e22d4d89b8b043b
sepi <sepi@ciklum.com>
2016-12-26 12:09:57 +0200

    # mac support added

--- webdriver_manager/config.py
+++ webdriver_manager/config.py
@@ -1,2 +1,2 @@
-mozila_latest_release = "https://api.github.com/repos/mozilla/geckodriver/releases/latest?acess_tocken=565553fa74cec4a901fc49bccf294d7814353deb"
+mozila_latest_release = "https://api.github.com/repos/mozilla/geckodriver/releases/latest"
 mozila_release_tag = "https://api.github.com/repos/mozilla/geckodriver/releases/tags/{0}"
