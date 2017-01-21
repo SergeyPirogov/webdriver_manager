6f6d9f79a5e2698741ac4c305a94ba4449e87226
sergey <sergey@gmail.com>
2017-01-21 20:45:38 +0200

    test config tests fix

--- tests/test_config.py
+++ tests/test_config.py
@@ -8,14 +8,14 @@ expect_gh_token = ''
 
 
 def test_config_with_deafault_params():
-    config = Configuration(config_folder=os.path.dirname(__file__), section="GeckoDriver")
+    config = Configuration(config_folder=os.path.dirname(__file__), section="GeckoDriver", file_name="config.ini")
     assert config.driver_latest_release_url == expect_mz_latest
     assert config.mozila_release_tag == expect_mz_tag
     assert config.gh_token == expect_gh_token
 
 
 def test_config_variables_with_default_params():
-    config = Configuration(section="GeckoDriver")
+    config = Configuration(section="GeckoDriver", file_name="",config_folder="")
     assert config.gh_token == ''
     assert config.driver_latest_release_url == expect_mz_latest
     assert config.mozila_release_tag == expect_mz_tag
