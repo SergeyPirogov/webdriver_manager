c5b72cb22f98077450429ffa98d6126acd40de86
sergey <sergey@gmail.com>
2017-03-10 22:32:15 +0200

    DriverManager old style class error fix

--- webdriver_manager/manager.py
+++ webdriver_manager/manager.py
@@ -2,7 +2,7 @@ from webdriver_manager.cache import CacheManager
 from webdriver_manager import config
 
 
-class DriverManager:
+class DriverManager(object):
     def __init__(self):
         self._file_manager = CacheManager(to_folder=config.folder, dir_name=config.folder)
 
