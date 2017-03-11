50754ac5986187ce78623ccd8c75f66a0a338f7f
Serhii Pirohov <sergio_89@ukr.net>
2017-03-11 17:32:42 +0200

    Pipenv added instead of requirements.txt (#16)
    * types added
    
    * types error fixed
    
    * DriverManager old style class error fix
    
    * cache types added
    
    * types adde to microsoft
    
    * tox pipenv
    
    * tox pipenv
    
    * change
    
    * Pipfile pypi url fix
    
    * Pipfile.lock updated
    
    * tox pytest and codecov added

--- webdriver_manager/chrome.py
+++ webdriver_manager/chrome.py
@@ -6,9 +6,9 @@ from webdriver_manager import utils
 
 
 class ChromeDriverManager(DriverManager):
-    def __init__(self, version=None,
-                 os_type=utils.os_type()):
-        DriverManager.__init__(self)
+    def __init__(self,version=None,os_type=utils.os_type()):
+        # type: (str, str) -> None
+        super(ChromeDriverManager, self).__init__()
         # there is no driver with 64 bit
         if os_type == "win64":
             os_type = "win32"
@@ -16,6 +16,7 @@ class ChromeDriverManager(DriverManager):
                                    os_type=os_type)
 
     def install(self):
+        # type: () -> str
         bin_file = self._file_manager.download_driver(self.driver)
         os.chmod(bin_file.path, 0o755)
         return bin_file.path
--- webdriver_manager/firefox.py
+++ webdriver_manager/firefox.py
@@ -7,9 +7,11 @@ from webdriver_manager import utils
 
 class GeckoDriverManager(DriverManager):
     def __init__(self, version=None, os_type=utils.os_name()):
-        DriverManager.__init__(self)
+        # type: (str, str) -> GeckoDriverManager
+        super(GeckoDriverManager, self).__init__()
         self.driver = GeckoDriver(version=version,
                                   os_type=os_type)
 
     def install(self):
+        # type: () -> str
         return self._file_manager.download_driver(self.driver).path
