508f73f93abce7e315d97ed19cee18ac68bd776e
sergey <sergio_89@ukr.net>
2019-10-19 15:06:24 +0300

    prepare to phantomjs deprecation

--- tests/test_edge_driver.py
+++ tests/test_edge_driver.py
@@ -1,64 +1,11 @@
 import os
+
 import pytest
-import sys
 
-from tests.test_cache import cache
-from tests.test_chrome_driver import delete_cache
-from webdriver_manager.driver import EdgeDriver
 from webdriver_manager.microsoft import EdgeDriverManager
-from selenium import webdriver
-
-PATH = '.'
-
-def delete_old_install(path=None):
-    if path is None:
-        delete_cache()
-    else:
-        path = os.path.abspath(path)
-        try:
-            os.remove(os.path.join(path, 'phantomjs.exe'))
-            print(os.path.join(path, 'phantomjs.exe'))
-            for file in os.listdir(path):
-                if 'phantomjs' in file and not os.path.isfile(file):
-                    if 'phantomjs.exe' in os.listdir(os.path.join(path, file, 'bin')):
-                        shutil.rmtree(os.path.join(path, file))
-                        print(os.path.join(path, file))
-                elif 'phantomjs' in file and file.endswith('.zip'):
-                        os.remove(os.path.join(path, file))
-                        print(os.path.join(path, file))
-        except:
-            pass
-
-@pytest.mark.skipif(sys.platform != 'win32',
-                    reason="run only on windows")
-@pytest.mark.parametrize('path', [PATH, None])
-def test_edge_manager_with_selenium(path):
-    delete_old_install(path)
-    driver_path = EdgeDriverManager().install(path)
-    dr = webdriver.Edge(driver_path)
-    dr.quit()
 
 
-@pytest.mark.skipif(sys.platform != 'win32',
-                    reason="run only on windows")
-@pytest.mark.parametrize('path', [PATH, None])
+@pytest.mark.parametrize('path', [".", None])
 def test_edge_manager_with_selenium_cache(path):
-    driver_path = EdgeDriverManager().install(path)
-    dr = webdriver.Edge(driver_path)
-    dr.quit()
-
-
-def test_can_download_edge_driver_binary():
-    edge_driver = EdgeDriver("latest", "win")
-    edge_driver_bin = cache.download_binary(edge_driver)
-    assert edge_driver_bin.name == u'MicrosoftWebDriver'
-    assert os.path.exists(edge_driver_bin.path)
-
-
-def test_can_user_cached_edge_driver_binary():
-    delete_cache()
-    edge_driver = EdgeDriver("latest", "win")
-    cache.download_binary(edge_driver)
-    edge_driver_bin = cache.download_binary(edge_driver)
-    assert edge_driver_bin.name == u'MicrosoftWebDriver'
-    assert os.path.exists(edge_driver_bin.path)
+    driver_path = EdgeDriverManager(os_type="win32").install(path)
+    assert os.path.exists(driver_path)
--- tests/test_phantomjs_manager.py
+++ tests/test_phantomjs_manager.py
@@ -6,59 +6,25 @@ from selenium import webdriver
 from tests.test_cache import cache, delete_cache
 from webdriver_manager.phantomjs import PhantomJsDriverManager
 
-PATH = '.'
-
-
-def delete_old_install(path=None):
-    if path is None:
-        delete_cache()
-    else:
-        path = os.path.abspath(path)
-        for f in os.listdir(path):
-            if f.startswith("phantomjs-"):
-                try:
-                    os.remove(os.path.join(path, f))
-                except:
-                    pass
-
-
-
-def test_phantomjs_manager_with_specific_version():
-    bin = PhantomJsDriverManager("2.0.0", os_type="win32").install()
-    assert os.path.exists(bin)
-
-
-@pytest.mark.parametrize('path', [PATH, None])
-def test_phantomjs_manager_with_latest_version(path):
-    bin = PhantomJsDriverManager().install(path)
-    assert os.path.exists(bin)
-
-
-def test_phantomjs_manager_with_wrong_version():
-    with pytest.raises(ValueError) as ex:
-        delete_old_install()
-        PhantomJsDriverManager("0.2", os_type="win32").install()
-    assert ex.value.args[0] == "There is no such driver phantomjs with version 0.2 " \
-                               "by https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-0.2-windows.zip"
-
-
-def test_phantomjs_manager_with_selenium():
-    delete_old_install()
-    driver_path = PhantomJsDriverManager().install()
-    webdriver.PhantomJS(driver_path)
-
-
-@pytest.mark.parametrize('path', [PATH, None])
-def test_phantomjs_manager_cached_driver_with_selenium(path):
-    PhantomJsDriverManager().install(path)
-    webdriver.PhantomJS(PhantomJsDriverManager().install(path))
-
-
-@pytest.mark.parametrize('os_type', ['win32', 'win64'])
-def test_can_get_phantomjs_for_windows(os_type):
-    delete_cache()
-    path = PhantomJsDriverManager(os_type=os_type).install()
-    assert os.path.exists(path)
+# PATH = '.'
+#
+#
+# def test_phantomjs_manager_with_specific_version():
+#     bin = PhantomJsDriverManager("2.0.0", os_type="win32").install()
+#     assert os.path.exists(bin)
+#
+#
+# @pytest.mark.parametrize('path', [PATH, None])
+# def test_phantomjs_manager_with_latest_version(path):
+#     bin = PhantomJsDriverManager(version="2.1.1").install(path)
+#     assert os.path.exists(bin)
+#
+#
+# @pytest.mark.parametrize('os_type', ['win32', 'win64'])
+# def test_can_get_phantomjs_for_windows(os_type):
+#     delete_cache()
+#     path = PhantomJsDriverManager(os_type=os_type).install()
+#     assert os.path.exists(path)
 
 
 @pytest.mark.parametrize('os_type', ['linux32', 'linux64'])
