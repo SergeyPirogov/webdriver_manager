2126a41a09c49780359f543811d8203e3c77161d
Serhii Pirohov <semen4ik20@gmail.com>
2020-05-16 15:16:18 +0300

    Update test_silent_logs.py

--- tests/test_silent_logs.py
+++ tests/test_silent_logs.py
@@ -5,5 +5,5 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 
 def test_chrome_manager_with_logs_level_0_set_locally():
-    bin = ChromeDriverManager("2.26", log_level=logging.INFO).install()
+    bin = ChromeDriverManager("2.26", log_level=0).install()
     assert os.path.exists(bin)
