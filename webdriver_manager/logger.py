8131021b12abaac2b86ae53eefb3677fd638ef54
SergeyPirogov <semen4ik20@gmail.com>
2020-05-22 13:22:04 +0300

    log -> print

--- webdriver_manager/logger.py
+++ webdriver_manager/logger.py
@@ -6,17 +6,18 @@ loggers = {}
 
 def log(text, level=logging.INFO, name="WDM"):
     log_level = os.getenv('WDM_LOG_LEVEL')
-    if log_level:
-        level = int(log_level)
-    if loggers.get(name):
-        loggers.get(name).info(text)
-    else:
-        _logger = logging.getLogger(name)
-
-        handler = logging.StreamHandler()
-        formatter = logging.Formatter('[%(name)s] - %(message)s')
-        handler.setFormatter(formatter)
-        _logger.addHandler(handler)
-        _logger.setLevel(level)
-        loggers[name] = _logger
-        _logger.info(text)
+    if log_level and log_level != 0:
+        print(f"[WDM] - {text}")
+    #     level = int(log_level)
+    # if loggers.get(name):
+    #     loggers.get(name).info(text)
+    # else:
+    #     _logger = logging.getLogger(name)
+    #
+    #     handler = logging.StreamHandler()
+    #     formatter = logging.Formatter('[%(name)s] - %(message)s')
+    #     handler.setFormatter(formatter)
+    #     _logger.addHandler(handler)
+    #     _logger.setLevel(level)
+    #     loggers[name] = _logger
+    #     _logger.info(text)
