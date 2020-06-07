acd19d99bf8c22036633e7e2f64e35c832ad61c9
sergey_pirogov <semen4ik20@gmail.com>
2020-06-07 11:12:16 +0100

    file class added

--- webdriver_manager/utils.py
+++ webdriver_manager/utils.py
@@ -9,6 +9,37 @@ import requests
 from webdriver_manager.logger import log
 
 
+class File(object):
+
+    def __init__(self, stream):
+        self.content = stream.content
+        self.__stream = stream
+        self.__temp_name = "driver"
+
+    @property
+    def filename(self) -> str:
+        try:
+            filename = re.findall("filename=(.+)", self.__stream.headers["content-disposition"])[0]
+        except KeyError:
+            filename = f"{self.__temp_name}.zip"
+        except IndexError:
+            filename = f"{self.__temp_name}.exe"
+
+        if '"' in filename:
+            filename = filename.replace('"', "")
+
+        return filename
+
+
+def save_file(file: File, directory: str):
+    os.makedirs(directory, exist_ok=True)
+
+    archive_path = f"{directory}{os.sep}{file.filename}"
+    with open(archive_path, "wb") as code:
+        code.write(file.content)
+    return archive_path
+
+
 class OSType(object):
     LINUX = "linux"
     MAC = "mac"
@@ -60,6 +91,13 @@ def download_driver(url):
     response = requests.get(url, stream=True)
     validate_response(response)
     return response
+
+
+def download_file(url: str) -> File:
+    log(f"Trying to download new driver from {url}")
+    response = requests.get(url, stream=True)
+    validate_response(response)
+    return File(response)
 
 
 def get_date_diff(date1, date2, date_format):
