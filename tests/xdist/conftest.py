ecda56ad06fb048501b1f7f2d860d3184399bf0d
s.pirohov <sergey.pirogov@gmail.com>
2022-05-23 15:37:39 +0300

    xdist test

--- tests/xdist/conftest.py
+++ tests/xdist/conftest.py
@@ -1,7 +1,11 @@
-import pytest
+import os
 
+import pytest
+import logging
 
 @pytest.fixture()
 def user_account(worker_id):
     """ use a different account in each xdist worker """
-    print(f"worker id: {worker_id}")
+    os.environ['WDM_LOCAL'] = '1'
+
+    logging.info(f"worker id: {worker_id}")
--- tests/xdist/test_cuncurent_2.py
+++ tests/xdist/test_cuncurent_2.py
@@ -1,3 +1,6 @@
+import logging
+import os
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 
@@ -5,6 +8,8 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 
 def test_chrome_manager_with_selenium():
+    logging.info(os.environ.get("PYTEST_XDIST_WORKER"))
+
     driver_path = ChromeDriverManager().install()
     driver = webdriver.Chrome(service=Service(driver_path))
     driver.get("http://automation-remarks.com")
