165c64adf65a8f36a0994e1de43615c54f954e5f
Andrew Vaughan <hello@andrewvaughan.io>
2022-07-10 14:21:16 -0400

    Update logger.py
    I believe your intent with `WD_LOG` is to allow users to control what level log messages are written as. As such, using `logging.setLevel()` does not do this (it only will set the level at which messages are culled). This change should fix what you intend to happen appropriately.

--- webdriver_manager/core/logger.py
+++ webdriver_manager/core/logger.py
@@ -4,7 +4,6 @@ from webdriver_manager.core.config import wdm_log_level
 
 __logger = logging.getLogger("WDM")
 
-__logger.setLevel(wdm_log_level())
 handler = logging.StreamHandler()
 formatter = logging.Formatter("[%(name)s] - %(message)s")
 handler.setFormatter(formatter)
@@ -13,4 +12,4 @@ __logger.addHandler(handler)
 
 def log(text):
     """Emitting the log message."""
-    __logger.info(text)
+    __logger.log(wdm_log_level, text)
