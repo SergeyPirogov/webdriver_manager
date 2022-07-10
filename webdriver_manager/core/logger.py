9663db3b0421a4b985875868fee4cf66d664b92c
Andrew Vaughan <hello@andrewvaughan.io>
2022-07-10 14:18:50 -0400

    Update logger.py
    Dependency modules should never set up their own log handlers - let parent projects do this.

--- webdriver_manager/core/logger.py
+++ webdriver_manager/core/logger.py
@@ -5,11 +5,6 @@ from webdriver_manager.core.config import wdm_log_level
 __logger = logging.getLogger("WDM")
 
 __logger.setLevel(wdm_log_level())
-handler = logging.StreamHandler()
-formatter = logging.Formatter("[%(name)s] - %(message)s")
-handler.setFormatter(formatter)
-__logger.addHandler(handler)
-
 
 def log(text):
     """Emitting the log message."""
