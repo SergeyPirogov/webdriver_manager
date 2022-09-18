f320c7ddc827d428815faa3bec5fb282e56d7656
DAKExDUCK <arsengabdulin228@gmail.com>
2022-09-18 13:36:22 +0600

    add option disable progress bar by env variable

--- webdriver_manager/core/config.py
+++ webdriver_manager/core/config.py
@@ -29,5 +29,13 @@ def wdm_log_level():
         return default_level
 
 
+def wdm_progress_bar():
+    default_level = 1
+    try:
+        return int(os.getenv("WDM_PROGRESS_BAR", default_level))
+    except Exception:
+        return default_level
+
+
 def get_xdist_worker_id():
     return os.getenv("PYTEST_XDIST_WORKER", '')
