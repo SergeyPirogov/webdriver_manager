575c33c79106e88cabe09beada765352f2b01a8f
punitarani <punitsai36@gmail.com>
2022-09-19 06:57:58 -0700

    Replace FileHandler with StreamHandler in test_custom_logger

--- tests/test_custom_logger.py
+++ tests/test_custom_logger.py
@@ -1,20 +1,10 @@
 """tests.test_custom_logger.py"""
 
 import logging
-from pathlib import Path
 
 import pytest
 
 from webdriver_manager.core.logger import log, set_logger, __logger
-
-
-# Log file path
-log_file = Path(__file__).resolve().parents[1].joinpath(".wdm/logs/WDM_DEBUG.log")
-
-# Create the log file if it doesn't exist
-if not log_file.exists():
-    log_file.parent.mkdir(parents=True, exist_ok=True)
-    log_file.touch()
 
 # Cache the old logger to restore it later
 __old_logger = __logger
@@ -27,28 +17,25 @@ def create_logger():
     # Create a Custom Debug Logger
     logger = logging.getLogger("WDM-DEBUG")
     logger.setLevel(logging.DEBUG)
-    logger.addHandler(logging.FileHandler(log_file))
+    logger.addHandler(logging.StreamHandler())
 
     return logger
 
 
-def test_custom_logger(create_logger):
+def test_custom_logger(capsys, create_logger):
     """Test the custom logger."""
 
     # Set the custom logger
     set_logger(create_logger)
 
     # send a log message
-    log("This is a test log message")
+    log_msg = "This is a test log message from the custom logger"
+    log(log_msg)
 
-    # Check if the log file exists
-    assert log_file.exists()
-
-    # Check if the log file contains the log message
-    assert "This is a test log message" in log_file.read_text()
-
-    # Delete the contents of the log file
-    open(log_file, "w").close()
+    # Check if the log message is in the output
+    captured = capsys.readouterr()
+    assert log_msg in captured.err
+    capsys.close()
 
     # Restore the old logger
     set_logger(__old_logger)
