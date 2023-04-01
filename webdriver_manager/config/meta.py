e9caaf5359612560b8b93fb63ef92ce6b740f4ad
HansBug <hansbug@buaa.edu.cn>
2023-04-01 12:46:23 +0800

    release(hansbug): release use version v0.1.0

--- webdriver_manager/config/meta.py
+++ webdriver_manager/config/meta.py
@@ -7,7 +7,7 @@ Overview:
 __TITLE__ = "hf-webdriver-manager"
 
 #: Version of this project.
-__VERSION__ = "0.0.2"
+__VERSION__ = "0.1.0"
 
 #: Short description of the project, will be included in ``setup.py``.
 __DESCRIPTION__ = 'Library provides the way to automatically manage drivers for different browsers.'
