185e872948900cc0416bb50a2e3ce6fffa6dbaf9
David Engelmann <david+4113@satisfilabs.com>
2023-07-19 10:16:09 -0400

    fix filepath creation in File object

--- webdriver_manager/core/utils.py
+++ webdriver_manager/core/utils.py
@@ -21,9 +21,15 @@ class File(object):
         try:
             # filename = re.findall('filename.*"(.+)"', self.__stream.headers["content-disposition"])[0] # TODO delete this commented code after testing new block
             content = self.__stream.headers["content-disposition"]
-            content_disposition_list = re.split(";", content)
-            filenames = [re.findall(self.__regex_filename, element) for element in content_disposition_list]
-            filename = next(filter(None, next(filter(None, next(filter(None, filenames))))))  # type: ignore
+            if "attachment" in content.lower():
+                if "octe" in self.__stream.headers["content-type"]:
+                    filename = f"{self.__temp_name}.zip"
+                else:
+                    raise Exception("Unknown Attachment Type")
+            else:
+                content_disposition_list = re.split(";", content)
+                filenames = [re.findall(self.__regex_filename, element) for element in content_disposition_list]
+                filename = next(filter(None, next(filter(None, next(filter(None, filenames))))))  # type: ignore
         except KeyError:
             filename = f"{self.__temp_name}.zip"
         except (IndexError, StopIteration):
