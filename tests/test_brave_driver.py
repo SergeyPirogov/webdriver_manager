d39dc83d166aa1e0cd21205cc4128d2cc4aa52de
s.pirohov <sergey.pirogov@gmail.com>
2023-07-22 17:47:13 +0300

    fix tests

--- tests/test_brave_driver.py
+++ tests/test_brave_driver.py
@@ -8,7 +8,6 @@ from webdriver_manager.core.logger import log
 from webdriver_manager.core.utils import ChromeType, os_name, OSType
 
 
-@pytest.mark.skip(reason='Brave version is strange on CI')
 def test_driver_with_ssl_verify_disabled_can_be_downloaded(ssl_verify_enable):
     os.environ['WDM_SSL_VERIFY'] = '0'
     custom_path = os.path.join(
@@ -29,7 +28,7 @@ def test_brave_manager_with_specific_version():
     bin_path = ChromeDriverManager("87.0.4280.88", chrome_type=ChromeType.BRAVE).install()
     assert os.path.exists(bin_path)
 
-
+@pytest.mark.skip(reason='Brave version is strange on CI')
 def test_brave_manager_with_selenium():
     binary_location = {
         OSType.LINUX: "/usr/bin/brave-browser",
