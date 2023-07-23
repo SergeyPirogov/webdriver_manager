2936a8a0a1c24f78b3a861a962015506ac7bc0e9
s.pirohov <sergey.pirogov@gmail.com>
2023-07-23 18:22:52 +0300

    os_type deprecation for Driver class

--- webdriver_manager/core/driver.py
+++ webdriver_manager/core/driver.py
@@ -34,9 +34,6 @@ class Driver(object):
     def get_name(self):
         return self._name
 
-    # def get_os_type(self):
-    #     return self._os_system_manager.get_os_type()
-
     def get_driver_download_url(self, os_type):
         return f"{self._url}/{self.get_driver_version_to_download()}/{self._name}_{os_type}.zip"
 
