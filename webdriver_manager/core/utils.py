3e9c719e2e40b698b7e3393d1b7672a29762ae4e
gamithaKalharaW <136357643+gamithaKalharaW@users.noreply.github.com>
2024-07-21 10:39:36 +0530

    updated powershell->pwsh

--- webdriver_manager/core/utils.py
+++ webdriver_manager/core/utils.py
@@ -7,7 +7,8 @@ import subprocess
 def get_date_diff(date1, date2, date_format):
     a = datetime.datetime.strptime(date1, date_format)
     b = datetime.datetime.strptime(
-        str(date2.strftime(date_format)), date_format)
+        str(date2.strftime(date_format)), date_format
+    )
 
     return (b - a).days
 
@@ -18,8 +19,9 @@ def linux_browser_apps_to_cmd(*apps: str) -> str:
     Result command example:
         chromium --version || chromium-browser --version
     """
-    ignore_errors_cmd_part = " 2>/dev/null" if os.getenv(
-        "WDM_LOG_LEVEL") == "0" else ""
+    ignore_errors_cmd_part = (
+        " 2>/dev/null" if os.getenv("WDM_LOG_LEVEL") == "0" else ""
+    )
     return " || ".join(f"{i} --version{ignore_errors_cmd_part}" for i in apps)
 
 
@@ -27,20 +29,22 @@ def windows_browser_apps_to_cmd(*apps: str) -> str:
     """Create analogue of browser --version command for windows."""
     powershell = determine_powershell()
 
-    first_hit_template = """$tmp = {expression}; if ($tmp) {{echo $tmp; Exit;}};"""
+    first_hit_template = (
+        """$tmp = {expression}; if ($tmp) {{echo $tmp; Exit;}};"""
+    )
     script = "$ErrorActionPreference='silentlycontinue'; " + " ".join(
         first_hit_template.format(expression=e) for e in apps
     )
 
-    return f'{powershell} -NoProfile "{script}"'
+    return f'{powershell} -NoProfile -Command "{script}"'
 
 
 def read_version_from_cmd(cmd, pattern):
     with subprocess.Popen(
-            cmd,
-            stdout=subprocess.PIPE,
-            stdin=subprocess.DEVNULL,
-            shell=True,
+        cmd,
+        stdout=subprocess.PIPE,
+        stdin=subprocess.DEVNULL,
+        shell=True,
     ) as stream:
         stdout = stream.communicate()[0].decode()
         version = re.search(pattern, stdout)
@@ -52,11 +56,11 @@ def determine_powershell():
     """Returns "True" if runs in Powershell and "False" if another console."""
     cmd = "(dir 2>&1 *`|echo CMD);&<# rem #>echo powershell"
     with subprocess.Popen(
-            cmd,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.DEVNULL,
-            stdin=subprocess.DEVNULL,
-            shell=True,
+        cmd,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.DEVNULL,
+        stdin=subprocess.DEVNULL,
+        shell=True,
     ) as stream:
         stdout = stream.communicate()[0].decode()
-    return "" if stdout == "powershell" else "powershell"
+    return "" if stdout == "powershell" else "pwsh"
